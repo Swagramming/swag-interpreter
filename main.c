75ef1a33bc7f7adf3bb5f3879debfea92a603c00
kimir <kimir@localhost.localdomain>
2014-07-15 17:19:14 +0200

    added scrollbars, +vexpand for editor/viewer

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 int main(int argc, char* argv[])
 {
-	GtkWidget *window,*editor,*viewer, *vbox, *hbox, *button;
+	GtkWidget *window,*editor,*viewer, *vbox, *hbox, *button, *subwin1, *subwin2;
 
 	gtk_init(&argc, &argv);
 	window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
@@ -19,7 +19,12 @@ int main(int argc, char* argv[])
     g_signal_connect (button, "clicked", G_CALLBACK (gtk_main_quit), NULL);
 
     gtk_box_pack_start(GTK_BOX(vbox), button, FALSE, FALSE, 0);
-    gtk_box_pack_start(GTK_BOX(vbox), hbox, FALSE, TRUE, 0); 
+    gtk_box_pack_start(GTK_BOX(vbox), hbox, FALSE, TRUE, 0);
+    
+    subwin1 = gtk_scrolled_window_new(FALSE, FALSE);
+    gtk_widget_set_vexpand(subwin1, TRUE);
+    subwin2 = gtk_scrolled_window_new(FALSE, FALSE);
+    gtk_widget_set_vexpand(subwin2, TRUE);
 
     editor = gtk_text_view_new();
     gtk_text_view_set_editable (GTK_TEXT_VIEW (editor), TRUE);
@@ -28,9 +33,12 @@ int main(int argc, char* argv[])
     viewer = gtk_text_view_new();
     gtk_text_view_set_editable (GTK_TEXT_VIEW (editor), FALSE);
     gtk_text_view_set_cursor_visible (GTK_TEXT_VIEW (editor), TRUE);
+    
+    gtk_container_add(GTK_CONTAINER(subwin1), editor);
+    gtk_container_add(GTK_CONTAINER(subwin2), viewer);
 
-    gtk_box_pack_start(GTK_BOX(hbox), viewer, TRUE, TRUE, 5);
-    gtk_box_pack_start(GTK_BOX(hbox), editor, TRUE, TRUE, 5);
+    gtk_box_pack_start(GTK_BOX(hbox), subwin2, TRUE, TRUE, 5);
+    gtk_box_pack_start(GTK_BOX(hbox), subwin1, TRUE, TRUE, 5);
 
 
     gtk_widget_show_all (window);
